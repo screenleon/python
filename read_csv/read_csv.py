9fc3895ccf8330fae9ef1c4fe3bbff1aafa8ff6f
screenleon <screen.leon@gmail.com>
2019-12-28 17:41:31 +0800

    rewrite csv file to regular
    remove "-" from the csv to make it can read from pandas

---
+++ read_csv/read_csv.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+# 讀取期交所的csv檔案，不正常。
+# data = pd.read_csv('op1216.csv', 'coding=Big5', engine='python')
+
+temp = ""
+
+with open('op1216.csv') as fp:
+    line = fp.readline()
+    temp = line.replace(" ", "")
+    line = fp.readline()           # ----------------------------- continue line 2
+    line = fp.readline()
+    while line:
+        temp += line.replace(" ", "")
+        line = fp.readline()
+
+# print(temp)
+
+with open('optest.csv', 'w') as fp:
+    fp.write(temp)
